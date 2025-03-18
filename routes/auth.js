4a674cad73f52664c0d9cbcc4a6f661d4fe5ad0f
jaime <jamesowiti24@gmail.com>
2025-03-18 17:26:27 +0300

    Vercel Test

--- routes/auth.js
+++ routes/auth.js
@@ -20,7 +20,8 @@ router.post("/login", async (req, res) => {
         expiresIn: "1h",
       }
     );
-
+    //test
+    
     res.json({ token });
   } catch (err) {
     res.status(500).json({ message: err.message });
