a17ba6f9de9168b8f6cda9ce5fb0c6058868a9b8
jaime <jamesowiti24@gmail.com>
2025-03-18 18:35:45 +0300

    Added Vercel Config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const router = require("./routes/users");
 require("dotenv").config();
 
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3001" }));
 app.use(express.json());
 app.use(express.json({ limit: "1mb" }));
 
@@ -24,5 +25,12 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/posts", require("./routes/posts"));
 app.use("/api/categories", require("./routes/categories"));
 
+
+app.get("/", (req, res) => {
+  res.send("Hello, Vercel!");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+export default app;
