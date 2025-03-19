283900f53a53a8242da8ea418b9f9c23edfc50ee
jaime <jamesowiti24@gmail.com>
2025-03-19 11:30:12 +0300

    Changed CORS

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors());
 app.use(express.json());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "1mb" }));
 
 // Connect to MongoDB
 mongoose
