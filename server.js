81a662961cc695b5c24d7cb6c8f0d27f885fb867
jaime <jamesowiti24@gmail.com>
2025-05-12 14:26:20 +0300

    Preventing cors

--- server.js
+++ server.js
@@ -7,7 +7,12 @@ require("dotenv").config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+    credentials: true, 
+  })
+);
 app.use(express.json());
 app.use(express.json({ limit: "1mb" }));
 
